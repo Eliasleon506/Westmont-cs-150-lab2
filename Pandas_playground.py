1ea61b8d83e399394896999c1da2ed411aea8049
eliasleon <leonelias506@gmail.com>
2025-01-22 13:50:20 -0800

    Done ;)

--- Pandas_playground.py
+++ Pandas_playground.py
@@ -1,3 +1,18 @@
 import pandas as pd
 
-print("Hello, pandas!")
+#print("Hello, pandas!")
+
+s = pd.Series(["c","s",1,5,0])
+#print(s)
+
+df = pd.DataFrame({"Professor": "Mike",
+                   'Classes':["cs-150", "cs-030", "senior-sem"],
+                   "Me": ["attending","tutor", "not yet;)" ]})
+
+#print(df)
+
+print(df.iloc[2,2])
+
+df.iloc[2,2] = "Oh no I am a senior !!"
+
+print(df)
